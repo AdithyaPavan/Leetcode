ad0ff298d25261a83444ee8c212e99feb661a98f
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2025-06-15 18:04:11 +0530

    Time: 1 ms (92.38%) | Memory: 41 MB (27.62%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxDiff(int num) {
+        String s = Integer.toString(num);
+        char maxReplace = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '9') {
+                maxReplace = s.charAt(i);
+                break;
+            }
+        }
+        String a = (maxReplace == ' ') ? s : s.replace(maxReplace, '9');
+
+        String b = s;
+        if (s.charAt(0) != '1') {
+            b = s.replace(s.charAt(0), '1');
+        } else {
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) != '0' && s.charAt(i) != '1') {
+                    b = s.replace(s.charAt(i), '0');
+                    break;
+                }
+            }
+        }
+
+        return Integer.parseInt(a) - Integer.parseInt(b);
+
+    }
+}
