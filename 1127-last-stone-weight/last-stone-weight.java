a3ed1aab27b48e5c6487c60ef0bd00d2d9fbd87d
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2025-10-29 10:17:48 +0530

    Time: 1 ms (98.62%) | Memory: 41.3 MB (34.77%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-       PriorityQueue<Integer>pq=new PriorityQueue<>(Comparator.reverseOrder()); 
+       PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->b-a); 
        for(int i:stones){
         pq.offer(i);
        }
