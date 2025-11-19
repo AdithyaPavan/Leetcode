10121cfe315fcad48fac5361d5bd826dcebfed5e
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2025-11-19 22:35:17 +0530

    Time: 11 ms (47.10%) | Memory: 45.6 MB (46.45%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -5,19 +5,19 @@ class Solution {
         int maxPos = Math.min(steps / 2 + 1, arrLen);
         long[][] dp = new long[steps + 1][maxPos];
 
-        dp[0][0] = 1;  // base case
+        dp[0][0] = 1;  
 
         for (int i = 1; i <= steps; i++) {
             for (int j = 0; j < maxPos; j++) {
 
-                // stay
+                
                 dp[i][j] = dp[i - 1][j];
 
-                // move left
+                
                 if (j > 0) 
                     dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
 
-                // move right
+             
                 if (j + 1 < maxPos)
                     dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % MOD;
             }
