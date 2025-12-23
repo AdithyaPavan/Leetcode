f61effc8784549f6f0565b02be21fca5c4b95750
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2025-12-23 11:36:10 +0530

    Time: 40 ms (90.50%) | Memory: 174.7 MB (54.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,22 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        int maxVal = 0, ans = 0;
-
-        for (int[] event : events) {
-            int start = event[0], end = event[1], value = event[2];
-
-            while (!pq.isEmpty() && pq.peek()[0] < start) {
-                maxVal = Math.max(maxVal, pq.poll()[1]);
+        Arrays.sort(events,(a,b)->a[0]-b[0]);
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        int res=0;
+        int top=0;
+        for(int e[]:events){
+            int s=e[0];
+            int en=e[1];
+            int val=e[2];
+            while(!pq.isEmpty()&& pq.peek()[1]<s){
+                top=Math.max(top,pq.poll()[2]);
             }
-
-            ans = Math.max(ans, maxVal + value);
-            pq.add(new int[]{end, value});
+            res=Math.max(res,top+val);
+            pq.offer(e);
         }
+        return res;
 
-        return ans;
     }
 }
